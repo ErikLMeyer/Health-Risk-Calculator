98b36c0131f7cddd8fb22323d3d3cb537c6e047c
ErikLMeyer <erikmeyer@lewisu.edu>
2021-03-08 13:44:25 -0600

    Wrote the Node server stuff. Still need to modify to serve image.

--- risk-calculator.js
+++ risk-calculator.js
@@ -1,3 +1,17 @@
 /*
     Node.js code goes in this file. -Erik
 */
+
+const http = require('http')
+const fs = require('fs')
+const port = process.env.PORT || 3000
+
+const server = http.createServer((req,res) => {
+   fs.readFile('index.html', (err, data) => {
+       res.writeHead(200, { 'Content-Type': 'text/html'})
+       res.end(data)
+   })
+  })
+  
+  server.listen(port, () => console.log(`server started on port ${port}; ` +
+    'press Ctrl-C to terminate....'))
