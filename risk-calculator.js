9f39fe768d096c0fca779e2e70290807c5c6e635
ErikLMeyer <erikmeyer@lewisu.edu>
2021-03-09 11:10:04 -0600

    Upgraded to Express

--- risk-calculator.js
+++ risk-calculator.js
@@ -2,16 +2,14 @@
     Node.js code goes in this file. -Erik
 */
 
-const http = require('http')
-const fs = require('fs')
+const express = require('express');
+const app = express();
 const port = process.env.PORT || 3000
 
-const server = http.createServer((req,res) => {
-   fs.readFile('index.html', (err, data) => {
-       res.writeHead(200, { 'Content-Type': 'text/html'})
-       res.end(data)
-   })
-  })
-  
-  server.listen(port, () => console.log(`server started on port ${port}; ` +
+app.use(express.static(__dirname + '/'));
+
+app.get('/', (req,res) => res.render('index'));
+
+
+app.listen(port, () => console.log(`server started on port ${port}; ` +
     'press Ctrl-C to terminate....'))
